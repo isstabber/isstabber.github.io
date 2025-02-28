0347e1348c95abc0807a8fc18e8e1d2c9e8fed06
isstabber <sta8r9@163.com>
2025-03-01 01:04:17 +0800

    Site updated: 2025-03-01 01:04:17

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
 var posts=["2025/02/28/hello-world/","2025/02/28/“文章”/"];function toRandomPost(){
-    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
+    window.location.href='/'+posts[Math.floor(Math.random() * posts.length)];
   };
