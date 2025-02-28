cb1c951071803982d8cc0f3e856c3e5c3d0642c1
isstabber <sta8r9@163.com>
2025-03-01 01:43:41 +0800

    Site updated: 2025-03-01 01:43:40

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/28/hello-world/"];function toRandomPost(){
+var posts=["2025/02/28/hello-world/","2025/03/01/CVE-2024-41592 vigor 栈溢出漏洞分析/"];function toRandomPost(){
     window.location.href='/'+posts[Math.floor(Math.random() * posts.length)];
   };
